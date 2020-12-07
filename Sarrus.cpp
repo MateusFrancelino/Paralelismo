3663d0ef33852c962ae086a202ef2aa9f5031bd6
MateusFrancelino <32208103+MateusFrancelino@users.noreply.github.com>
2020-12-07 20:40:01 -0300

    Update Sarrus.cpp

--- Sarrus.cpp
+++ Sarrus.cpp
@@ -2,13 +2,15 @@
 #include <stdio.h>
 #include <iostream>
 #include <time.h>
+#include <omp.h>
+#include<string>
 
 using namespace std;
 
 int sorteio(int maximo, int minimo = 0) {
     int i;
 
-    
+
 
     i = rand() % maximo + minimo;
 
@@ -18,55 +20,97 @@ int sorteio(int maximo, int minimo = 0) {
 
 
 int main() {
-
+    string texto;
     int matrix[3][3];
-    for (int exec = 0; exec < 31;exec++) {
-   
-        for (int linha = 0; linha < 3; linha++) {
-            for (int coluna = 0; coluna < 3; coluna++) {
-                matrix[linha][coluna] = sorteio(333);
-            }
-    }
-
     long long int ver = 0;
     long long int azul = 0;
-    int matrixsarrus [3][5];
-    for (int l = 0; l < 3; l++) {
-        for (int c = 0; c < 5; c++) {
-            if (c > 2) {
-                
-                matrixsarrus[l][c] = matrix[l][(c - 3)];
+    int matrixsarrus[3][5];
+    int total = 0;
+    long double tempo_execucao = 0.000000;
+    long long int determinante = 0;
+    long double inicio = 0.0;
+    long double final = 0.0;
+
+    for (int exec = 0; exec < 1; exec++) {
+        ver = 0;
+        azul = 0;
+        total = 0;
+        determinante = 0;
+        /*
+            for (int linha = 0; linha < 3; linha++) {
+                 for (int coluna = 0; coluna < 3; coluna++) {
+                     matrix[linha][coluna] = sorteio(333);
+                 }
+         }
+
+       */
+
+        matrix[0][0] = 1;
+        matrix[0][1] = 5;
+        matrix[0][2] = -2;
+        matrix[1][0] = 8;
+        matrix[1][1] = 3;
+        matrix[1][2] = 0;
+        matrix[2][0] = 4;
+        matrix[2][1] = -1;
+        matrix[2][2] = 2;
+
+        inicio = omp_get_wtime();
+
+
+            for (int l = 0; l < 3; l++) {
+
+                for (int c = 0; c < 5; c++) {
+
+                    if (c > 2) {
+
+                        matrixsarrus[l][c] = matrix[l][(c - 3)];
+                    }
+                    else {
+                        matrixsarrus[l][c] = matrix[l][c];
+                    }
+                }
+
             }
-            else {
-                matrixsarrus[l][c] = matrix[l][c];
-            }
-        }
         
-    }
-   
-    for (int l = 0; l < 3; l++) {
-        for (int c = 0; c < 5; c++) {
-            cout << matrixsarrus[l][c]<<"          ";
+
+
+
+
+
+
+            int soma_local = 0;
+            int sub_local = 0;
+
+
+            for (int coluna = 0; coluna < 3; coluna++) {
+                soma_local += matrixsarrus[0][coluna] * matrixsarrus[1][coluna + 1] * matrixsarrus[2][coluna + 2];
+           
+            }
+
+            for (int coluna = 4; coluna > 1; coluna--) {
+                sub_local -= matrixsarrus[0][coluna] * matrixsarrus[1][coluna - 1] * matrixsarrus[2][coluna - 2];
+                
+            }
+
+
+            determinante += soma_local + sub_local;
+
+        
+        final = omp_get_wtime();
+        for (int l = 0; l < 3; l++) {
+            for (int c = 0; c < 5; c++) {
+                cout << matrixsarrus[l][c] << "          ";
+            }
+            cout << endl << endl << endl << endl;
         }
-        cout << endl << endl << endl << endl;
+
     }
 
-    for (int coluna = 0; coluna < 3; coluna++) {
-        ver += matrixsarrus[0][coluna] * matrixsarrus[1][coluna + 1] * matrixsarrus[2][coluna + 2];
-    }
+    cout << "Determinante " << determinante << endl;
+    tempo_execucao += (final - inicio);
+    cout << (int)tempo_execucao << "     tempo_execucao" << tempo_execucao;
 
-    for (int coluna = 4; coluna > 1; coluna--) {
-        azul -= matrixsarrus[0][coluna] * matrixsarrus[1][coluna - 1] * matrixsarrus[2][coluna - 2];
-    }
-    cout << endl << endl << "resultado" << endl;
-    cout << ver;
-    cout << endl << endl << "resultado" << endl;
-    cout << azul;
 
-    long long int determinante = ver + azul;
-    cout << endl << endl << "determinante" << endl;
-    cout << determinante;
-    cout << endl << endl << endl << endl << endl << endl;
-    }
-    return 0;
+
 }
